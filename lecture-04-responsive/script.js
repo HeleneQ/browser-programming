b1167f30ae97154e6faf1e43f941ec9420467fbf
HeleneQ <helene.quernet@etu.estia.fr>
2026-02-03 22:05:56 +0200

    lecture 4 assignment

--- lecture-04-responsive/script.js
+++ lecture-04-responsive/script.js
@@ -18,3 +18,33 @@ function setTheme() {
         console.log("Theme changed to: Light Mode");
     }
 }
+
+const themeButton = document.getElementById('theme-toggle');
+if (themeButton) {
+    themeButton.addEventListener('click', setTheme);
+}
+
+console.log("Initial state - Dark Mode: " + isDarkMode);
+
+
+//Button to access quick contact information
+
+const contactEmail = "helene.quernet@edu.savonia.fi";
+
+function showContactInfo() {
+    // Console log #3: Log when contact info is requested
+    console.log("Contact quick action triggered - Email: " + contactEmail);
+    
+    // Show alert with contact information
+    alert(
+        "📧 Contact Hélène Quernet\n\n" +
+        "Finnish email: helene.quernet@edu.savonia.fi\n" +
+        "French email: helene.quernet@etu.estia.fr\n" +
+        "GitHub: github.com/HeleneQ"
+    );
+}
+
+const contactButton = document.getElementById('contact-btn');
+if (contactButton) {
+    contactButton.addEventListener('click', showContactInfo);
+}
